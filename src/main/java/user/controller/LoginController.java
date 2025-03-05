c7005b410e1196ca8ad984fbd36e938a68e641cf
donghwi <withu19@naver.com>
2025-03-05 15:33:54 +0900

    틀공유

---
+++ src/main/java/user/controller/LoginController.java
@@ -0,0 +1,9 @@
+package user.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+	
+}
